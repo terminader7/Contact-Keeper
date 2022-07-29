8c1e167c0068c71ac64d45f3070c8a94bb5f3732
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-07-29 09:00:34 -0700

    added page navigation links to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 import { IdCard } from "../../icons/IdCard";
 
 const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  background-color: #003699;
 `;
 
 const NavbarContentContainer = styled.div`
@@ -17,13 +20,38 @@ const NavbarContentContainer = styled.div`
   font-size: 25px;
 `;
 
-export const Navbar = ({ title, icon }) => {
+const NavbarLinksList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: absolute;
+  margin-left: 1720px;
+`;
+
+const NavbarLinkItem = styled.div`
+  :hover {
+    opacity: 0.7;
+  }
+  cursor: pointer;
+  display: flex;
+  transition: 0.5s;
+`;
+
+export const Navbar = ({ title }) => {
   return (
     <NavbarContainer className="navbar bg-primary">
       <NavbarContentContainer>
         <IdCard />
         {title}
       </NavbarContentContainer>
+      <NavbarLinksList>
+        <NavbarLinkItem>
+          <Link to="/">Home</Link>
+        </NavbarLinkItem>
+        <NavbarLinkItem>
+          <Link to="/about">About</Link>
+        </NavbarLinkItem>
+      </NavbarLinksList>
     </NavbarContainer>
   );
 };
