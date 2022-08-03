9bf78325ce3b7cac1b4ff0b5a1ff3c39508174c8
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-08-03 09:43:17 -0700

    Set up auth folder and intitial state for auth

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,25 @@ import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 
 import { ContactState } from "./context/contact/ContactState";
+import { AuthState } from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
