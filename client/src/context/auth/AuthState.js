051cbcca7db2bd3f36f86a46247670370e717899
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-08-05 11:46:07 -0700

    meant to be in prior commit

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -56,7 +56,7 @@ export const AuthState = (props) => {
   const logout = () => console.log("logout");
 
   //Clear Errors
-  const clearErrors = () => console.log("clearErrors");
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <authContext.Provider
