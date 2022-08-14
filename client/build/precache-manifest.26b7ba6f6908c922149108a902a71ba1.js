b583815a18995ca3ce932a2e09a361b2ac2422a1
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-08-14 14:36:31 -0700

    unignored build file

---
+++ client/build/precache-manifest.26b7ba6f6908c922149108a902a71ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab4db64b421022b379d5",
+    "url": "/static/css/main.f7490551.chunk.css"
+  },
+  {
+    "revision": "ab4db64b421022b379d5",
+    "url": "/static/js/main.ab4db64b.chunk.js"
+  },
+  {
+    "revision": "bf749b80e57d87666cdb",
+    "url": "/static/js/1.bf749b80.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "496506c031ba97ca55bc05d961c825a6",
+    "url": "/static/media/spinner.496506c0.gif"
+  },
+  {
+    "revision": "80437c1644e9f2d598d357c8d0768f58",
+    "url": "/index.html"
+  }
+];
